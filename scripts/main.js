6907153bbc6f26df9c798a29523d014d77378716
IgorChurin <49653471+IgorChurin@users.noreply.github.com>
2019-04-15 23:34:37 +0600

    1

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+﻿var myImage = document.querySelector('img');
+
+myImage.onclick = function() {        //функция смены картинки
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/header.png') {
+      myImage.setAttribute ('src','images/medal-test.png');
+    } else {
+      myImage.setAttribute ('src','images/header.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {  // Установка имени пользователя
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML  = 'Привет, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML  = 'Привет, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
